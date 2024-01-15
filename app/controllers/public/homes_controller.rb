e0daddc8a6f64b74792087a59e7bf7fa884daa6d
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-01-15 10:17:26 +0000

    adminログインの不具合修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,15 @@
 class Public::HomesController < ApplicationController
+  before_action :set_devise_mapping
+
   def top
 
+  end
+
+  private
+
+  def set_devise_mapping
+    @user = User.new
+    @devise_mapping = Devise.mappings[:user]
+    @resource_name = :user
   end
 end
